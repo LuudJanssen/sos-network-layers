a72a6fdaeee4cdcc91739b98b6f5462ff2714ff9
s145469 <luud-@live.nl>
2018-12-13 15:04:40 +0100

    Install webpack-env types

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,9 @@ const gameElement = document.body
 const game = new Game()
 
 game.bindTo(gameElement)
+
+if (module.hot) {
+    module.hot.dispose(() => {
+        window.location.reload()
+    })
+}
