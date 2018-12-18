d0be96446b60ea79ffb8e25a75f848322d0cb123
Ewijk <s.v.ewijk@student.tue.nl>
2018-12-18 10:39:15 +0100

    form + navbar html/css

---
+++ src/components/Form.vue
@@ -0,0 +1,20 @@
+<template>
+
+<form>
+  First name:<br>
+  <input type="text" name="firstname"><br>
+  Last name:<br>
+  <input type="text" name="lastname">
+  Address:<br>
+  <input type="text" name="address"><br>
+  Longitude:<br>
+  <input type="text" name="longitude">
+  Latitude:<br>
+  <input type="text" name="latitude">
+  Emergency priority:<br>
+  <input type="radio" name="low" value="low" checked> Low<br>
+  <input type="radio" name="neutral" value="neutral"> Neutral<br>
+  <input type="radio" name="high" value="high"> High
+</form> 
+
+</template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,54 @@
+
+<template>
+<div id = 'navitems'>
+     <div class="topnav">
+        <a class="active" href="#item1">{{item1}}</a>
+        <a href="#item2">{{item2}}</a>
+        <a href="#item3">{{item3}}</a>
+        <a href="#item4">{{item4}}</a>
+    </div> 
+</div>
+</template>
+
+<script>
+    var navitems = new Vue({
+        el: '#navitems',
+        data: {
+            item1: 'item1',
+            item2: 'item2',
+            item3: 'item3',
+            item4: 'item4',
+        }
+    })
+</script>
+
+<style>
+
+.topnav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+
+</style>
