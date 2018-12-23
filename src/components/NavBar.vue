1f16fb9609e3e0f41f896f8d9b5b6e6c0f3477b4
susanvew@hotmail.com <susanvew@hotmail.com>
2018-12-23 16:54:23 +0100

    Eindelijk een navigation bar!

---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <b-nav justified tabs class='nav'>
+            <b-nav-item active>Tab1</b-nav-item>
+            <b-nav-item>Tab2</b-nav-item>
+            <b-nav-item>Tab3</b-nav-item>
+            <b-nav-item>Tab4</b-nav-item>
+            <b-nav-item>Tab5</b-nav-item>
+            <b-nav-item>Tab6</b-nav-item>
+        </b-nav>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    name: 'NavBar',
+})
+</script>
+
+<style scoped>
+.nav {
+    padding-top: 20px;
+}
+
+</style>
+
+
