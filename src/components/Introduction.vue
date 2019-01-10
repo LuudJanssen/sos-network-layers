39de7d4c99662d5e82fbc55d0c713e16420206a4
luudjanssen <luud-@live.nl>
2019-01-10 13:48:07 +0100

    Create introduction island

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -4,9 +4,15 @@
 -->
 <template>
   <b-container fluid class="introduction">
-    <b-row align-v="center" align-h="center" class="introduction-buttons">
-      <b-col></b-col>
-      <b-col cols="2">
+    <b-row align-v="center" align-h="center" class="introduction-island">
+      <div class="water"></div>
+      <img class="sideburn-left" src="../assets/water-sideburn-left.svg">
+      <img class="sideburn-right" src="../assets/water-sideburn-right.svg">
+      <b-col cols="3"></b-col>
+      <b-col>
+        <img src="../assets/island-detail.svg">
+      </b-col>
+      <!-- <b-col cols="2">
         <b-button :variant="'primary'" @click="$emit('startLayer', GameLayers.PhysicalLayer)">
           Start Physical Layer Game
         </b-button>
@@ -15,8 +21,8 @@
         <b-button :variant="'primary'" @click="$emit('startLayer', GameLayers.ApplicationLayer)">
           Start Application Layer Game
         </b-button>
-      </b-col>
-      <b-col></b-col>
+      </b-col> -->
+      <b-col cols="3"></b-col>
     </b-row>
   </b-container>
 </template>
@@ -35,11 +41,36 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .introduction {
-  background-color: var(--light);
+  background: radial-gradient(ellipse at center, #f0f9ff 0%,#cbebff 47%,#a1dbff 100%);
   height: 100%;
 }
 
-.introduction-buttons {
+.introduction-island {
+  position: relative;
+  z-index: 1;
   height: 100%;
+}
+
+.water {
+  position: absolute;
+  background-color: #4194B5;
+  width: 100%;
+  height: 50%;
+  bottom: 0;
+  z-index: 0;
+}
+
+.sideburn-left {
+  position: absolute;
+  width: 25%;
+  left: 0;
+  bottom: 0;
+}
+
+.sideburn-right {
+  position: absolute;
+  width: 20%;
+  right: 0;
+  bottom: 0;
 }
 </style>
