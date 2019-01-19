88f042f50e633519dddf1fea5cee2c21e6d9b421
s145469 <luud-@live.nl>
2019-01-19 22:24:36 +0100

    Start store

--- src/Game.vue
+++ src/Game.vue
@@ -12,154 +12,188 @@
 
 <!-- This is the TypeScript part of the Game component. -->
 <script>
-import Vue from "vue";
-import Vuex from "vuex";
-import { Tab, colorOptions } from "./components/tabs";
+    import Vue from "vue";
+    import Vuex from "vuex";
+    import {Tab, colorOptions} from "./components/tabs";
 
-Vue.use(Vuex);
+    Vue.use(Vuex);
 
-export const ConnectionStatus = {
-  NO_CONNECTION: "NO_CONNECTION",
-  USABILITY_PROBLEMS: "USABILITY_PROBLEMS",
-  CONNECTED: "CONNECTED"
-};
+    export const ConnectionStatus = {
+        NO_CONNECTION: "NO_CONNECTION",
+        USABILITY_PROBLEMS: "USABILITY_PROBLEMS",
+        CONNECTED: "CONNECTED"
+    };
 
-const store = new Vuex.Store({
-  state: {
-    showTaskList: true,
-    connectionStatus: ConnectionStatus.NO_CONNECTION,
-    color: {
-      text1: "black",
-      text2: "black",
-      text3: "black",
-      header1: "black",
-      header2: "black",
-      header3: "black"
-    },
-    tabs: [
-      new Tab(
-        "tab1",
-        ["Island", "Home", "Welcome", "Hello"],
-        colorOptions,
-        "home"
-      ),
-      new Tab(
-        "tab2",
-        ["Reporting", "Ahhhh", "Report SOS", "Stuck"],
-        colorOptions,
-        "form"
-      ),
-      new Tab(
-        "tab3",
-        ["History", "About us", "We", "Info about us"],
-        colorOptions,
-        ""
-      ),
-      new Tab(
-        "tab4",
-        ["Core values", "Tell me more", "Interesting", "More"],
-        colorOptions,
-        ""
-      )
-    ],
-    tabsData: [
-      //since I cannot read values of the class properly, this is added to circumvent reading from the class
-      {
-        color: colorOptions[0],
-        text: "Island"
-      },
-      {
-        color: colorOptions[0],
-        text: "Island"
-      },
-      {
-        color: colorOptions[0],
-        text: "Island"
-      },
-      {
-        color: colorOptions[0],
-        text: "Island"
-      }
-    ],
-    form: {
-      showName: true,
-      showAd: true,
-      showLong: true,
-      showLat: true,
-      showPrio: true
-    },
-    taskList: {
-      textCheck: false,
-      sosCheck: false,
-      formCheck: false,
-      complete: false
-    }
-  },
-  mutations: {
-    toggle(state, field) {
-      if (field.substring(0, 6) === "showTa") {
-        state.showTaskList = !state.showTaskList;
-      } else if (field.substring(0, 6) === "showNa") {
-        state.form.showName = !state.form.showName;
-      } else if (field.substring(0, 6) === "showAd") {
-        state.form.showAd = !state.form.showAd;
-      } else if (field.substring(0, 6) === "showLo") {
-        state.form.showLong = !state.form.showLong;
-      } else if (field.substring(0, 6) === "showLa") {
-        state.form.showLat = !state.form.showlat;
-      } else if (field.substring(0, 6) === "showPr") {
-        state.form.showPrio = !state.form.showPrio;
-      }
-    },
-    changeColor(state, field) {
-      if (field.substring(0, 4) === "text") {
-        if (field === "text1") {
-          state.color.text1 =
-            state.color.text1 === "black" ? "yellow" : "black";
-        } else if (field === "text2") {
-          state.color.text2 =
-            state.color.text2 === "black" ? "yellow" : "black";
-        } else if (field === "text3") {
-          state.color.text3 =
-            state.color.text3 === "black" ? "yellow" : "black";
+    const connectionModule = {
+        namespaced: true,
+        state: {
+            lights: [true, false, true, false],
+            hadExplanation: false
+        },
+        getters: {
+            finished: state => state.lights.every(light => light === true)
         }
-      } else if (field.substring(0, 4) === "head") {
-        if (field === "header1") {
-          state.color.header1 =
-            state.color.header1 === "black" ? "yellow" : "black";
-        } else if (field === "header2") {
-          state.color.header2 =
-            state.color.header2 === "black" ? "yellow" : "black";
-        } else if (field === "header3") {
-          state.color.header3 =
-            state.color.header3 === "black" ? "yellow" : "black";
-        }
-      }
-    },
-    changeConnectionStatus(state, status) {
-      state.connectionStatus = status;
-    },
-    set(state, param) {
-      if (param.field.substring(0, 6) === "textCh") {
-        state.taskList.textCheck = param.value;
-      } else if (param.field.substring(0, 6) === "sosChe") {
-        state.taskList.sosCheck = param.value;
-      } else if (param.field.substring(0, 6) === "formCh") {
-        state.taskList.formCheck = param.value;
-      } else if (param.field.substring(0, 6) === "comple") {
-        state.taskList.complete = param.value;
-      }
-    }
-  }
-});
+    };
 
-/**
- * This is the component code itself. It only exposes the current layer and all game layers to the template.
- */
-export default {
-  name: "Game",
-  store,
-};
+    const usabilityModule = {
+        namespaced: true,
+        getters: {
+            finished: state => false
+        }
+    }
+
+    const store = new Vuex.Store({
+        modules: {
+            connection: connectionModule,
+            usability: usabilityModule
+        },
+        state: {
+            hadIntroduction: false,
+            showTaskList: true,
+            connectionStatus: ConnectionStatus.NO_CONNECTION,
+            color: {
+                text1: "black",
+                text2: "black",
+                text3: "black",
+                header1: "black",
+                header2: "black",
+                header3: "black"
+            },
+            tabs: [
+                new Tab(
+                    "tab1",
+                    ["Island", "Home", "Welcome", "Hello"],
+                    colorOptions,
+                    "home"
+                ),
+                new Tab(
+                    "tab2",
+                    ["Reporting", "Ahhhh", "Report SOS", "Stuck"],
+                    colorOptions,
+                    "form"
+                ),
+                new Tab(
+                    "tab3",
+                    ["History", "About us", "We", "Info about us"],
+                    colorOptions,
+                    ""
+                ),
+                new Tab(
+                    "tab4",
+                    ["Core values", "Tell me more", "Interesting", "More"],
+                    colorOptions,
+                    ""
+                )
+            ],
+            tabsData: [
+                //since I cannot read values of the class properly, this is added to circumvent reading from the class
+                {
+                    color: colorOptions[0],
+                    text: "Island"
+                },
+                {
+                    color: colorOptions[0],
+                    text: "Island"
+                },
+                {
+                    color: colorOptions[0],
+                    text: "Island"
+                },
+                {
+                    color: colorOptions[0],
+                    text: "Island"
+                }
+            ],
+            form: {
+                showName: true,
+                showAd: true,
+                showLong: true,
+                showLat: true,
+                showPrio: true
+            },
+            taskList: {
+                textCheck: false,
+                sosCheck: false,
+                formCheck: false,
+                complete: false
+            }
+        },
+        getters: {
+            connectionStatus: (state, getters) => {
+                console.log(state, getters)
+
+                if (getters['connection/finished']) {
+                    return ConnectionStatus.USABILITY_PROBLEMS
+                }
+
+                return ConnectionStatus.NO_CONNECTION
+            }
+        },
+        mutations: {
+            toggle(state, field) {
+                if (field.substring(0, 6) === "showTa") {
+                    state.showTaskList = !state.showTaskList;
+                } else if (field.substring(0, 6) === "showNa") {
+                    state.form.showName = !state.form.showName;
+                } else if (field.substring(0, 6) === "showAd") {
+                    state.form.showAd = !state.form.showAd;
+                } else if (field.substring(0, 6) === "showLo") {
+                    state.form.showLong = !state.form.showLong;
+                } else if (field.substring(0, 6) === "showLa") {
+                    state.form.showLat = !state.form.showlat;
+                } else if (field.substring(0, 6) === "showPr") {
+                    state.form.showPrio = !state.form.showPrio;
+                }
+            },
+            changeColor(state, field) {
+                if (field.substring(0, 4) === "text") {
+                    if (field === "text1") {
+                        state.color.text1 =
+                            state.color.text1 === "black" ? "yellow" : "black";
+                    } else if (field === "text2") {
+                        state.color.text2 =
+                            state.color.text2 === "black" ? "yellow" : "black";
+                    } else if (field === "text3") {
+                        state.color.text3 =
+                            state.color.text3 === "black" ? "yellow" : "black";
+                    }
+                } else if (field.substring(0, 4) === "head") {
+                    if (field === "header1") {
+                        state.color.header1 =
+                            state.color.header1 === "black" ? "yellow" : "black";
+                    } else if (field === "header2") {
+                        state.color.header2 =
+                            state.color.header2 === "black" ? "yellow" : "black";
+                    } else if (field === "header3") {
+                        state.color.header3 =
+                            state.color.header3 === "black" ? "yellow" : "black";
+                    }
+                }
+            },
+            changeConnectionStatus(state, status) {
+                state.connectionStatus = status;
+            },
+            set(state, param) {
+                if (param.field.substring(0, 6) === "textCh") {
+                    state.taskList.textCheck = param.value;
+                } else if (param.field.substring(0, 6) === "sosChe") {
+                    state.taskList.sosCheck = param.value;
+                } else if (param.field.substring(0, 6) === "formCh") {
+                    state.taskList.formCheck = param.value;
+                } else if (param.field.substring(0, 6) === "comple") {
+                    state.taskList.complete = param.value;
+                }
+            }
+        }
+    });
+
+    /**
+     * This is the component code itself. It only exposes the current layer and all game layers to the template.
+     */
+    export default {
+        name: "Game",
+        store,
+    };
 </script>
 
 <!-- 
@@ -167,12 +201,12 @@ export default {
   elements
 -->
 <style>
-#game {
-  width: 100%;
-  height: 100%;
-}
+  #game {
+    width: 100%;
+    height: 100%;
+  }
 
-p, h1, h2, h3, h4, h5, h6, div {
-  font-family: "Montserrat", Helvetica, Arial, sans-serif;
-}
+  p, h1, h2, h3, h4, h5, h6, div {
+    font-family: "Montserrat", Helvetica, Arial, sans-serif;
+  }
 </style>
